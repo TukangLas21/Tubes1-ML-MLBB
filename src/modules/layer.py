fix: bce derivative, gradient outputs, and softmax deriv

src/modules/layer.py
@@ -38,8 +38,8 @@ class Layer:
         else:
             dz = d_a * type(self.activation_fn).derivative(self.z)
 
-        self.dW = np.dot(self.input.T, dz)
-        self.db = np.sum(dz, axis=0, keepdims=True)
+        self.dW = np.dot(self.input.T, dz) / self.input.shape[0]
+        self.db = np.sum(dz, axis=0, keepdims=True) / self.input.shape[0]
 
         # pass gradient to previous layer
         return np.dot(dz, self.W.T)

src/modules/lossfunction.py
@@ -30,11 +30,12 @@ class BinaryCrossEntropy(LossFunction):
     @staticmethod
     def derivative(y_true, y_pred, epsilon=1e-15):
         y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
-        return (y_pred - y_true) / y_true.shape[0]
+        return (1 / y_true.shape[0]) * (y_pred - y_true) / (y_pred * (1 - y_pred) + epsilon)
     
 class CategoricalCrossEntropy(LossFunction):
     @staticmethod
     def loss(y_true, y_pred):
+        y_pred = np.clip(y_pred, 1e-15, 1 - 1e-15)
         return -np.mean(np.sum(y_true * np.log(y_pred), axis=1))
     
     @staticmethod