4111440f9bc81f8878099680610404510e8afbc4
TukangLas21 <19623160@std.stei.itb.ac.id>
2026-03-15 15:45:22 +0700

    feat: integrate with model from main

--- src/modules/activationfunction.py
+++ src/modules/activationfunction.py
@@ -12,6 +12,7 @@ class ActivationFunction(ABC):
     def derivative(z):
         pass
 
+
 class Linear(ActivationFunction):
     @staticmethod
     def activate(z):
@@ -20,13 +21,54 @@ class Linear(ActivationFunction):
     @staticmethod
     def derivative(z):
         return np.ones_like(z)
-
-# TODO: Implement
-class Softmax(ActivationFunction):
+    
+    
+class Sigmoid(ActivationFunction):
     @staticmethod
     def activate(z):
-        return 0
+        return 1 / (1 + np.exp(-z))
+    
+    @staticmethod
+    def derivative(z):
+        forward_val = Sigmoid.activate(z)
+        return forward_val * (1 - forward_val)
+    
+    
+class ReLU(ActivationFunction):
+    @staticmethod
+    def activate(z):
+        return np.maximum(0, z)
 
     @staticmethod
     def derivative(z):
-        return 0
+        return (z > 0)
+    
+    
+class Tanh(ActivationFunction):
+    @staticmethod
+    def activate(z):
+        return np.tanh(z)
+
+    @staticmethod
+    def derivative(z):
+        return 1 - np.tanh(z) ** 2    
+
+
+class Softmax(ActivationFunction):
+    @staticmethod
+    def activate(z):
+        exp_z = np.exp(z - np.max(z, axis=-1, keepdims=True))
+        return exp_z / np.sum(exp_z, axis=-1, keepdims=True)
+
+    @staticmethod
+    def derivative(z):
+        softmax_output = Softmax.activate(z)
+        n, C = softmax_output.shape
+        
+        jacobian_matrices = np.zeros((n, C, C))
+        
+        for i in range(n):
+            softmax_output_i = softmax_output[i]
+            jacobian_matrices[i] = np.diag(softmax_output_i) - np.outer(softmax_output_i, softmax_output_i)
+        
+        return jacobian_matrices
